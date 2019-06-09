95b70e8dda4b80d99d651efa56740b68bd1c30f6
Piotr <48599434+Fla-sh@users.noreply.github.com>
2019-06-09 15:51:14 +0200

    Create stringi.cpp

---
+++ Zaliczenie/stringi.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	
+	char napis1[100] = "napis1";
+	char napis2[100] = "napis2";
+
+	printf("%s %s \n", napis1, napis2);
+
+	char napis_z_konsoli[20];
+	scanf_s("%s", napis_z_konsoli, 20);
+	printf("%s\n", napis_z_konsoli);
+
+	strcat_s(napis1, napis2);
+	printf("%s", napis1);
+
+	char* miejsce = strchr(napis1, 'a');
+	printf("%d", miejsce);
+
+	if (strcmp(napis1, napis2) == 0) {
+		printf("Takie same");
+	}
+
+	return 0;
+
+}
