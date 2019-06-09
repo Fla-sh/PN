6640b55e97f136209b861d6f6f69d7e4f4107f3b
Piotr <48599434+Fla-sh@users.noreply.github.com>
2019-06-09 15:06:02 +0200

    Create switch.cpp

---
+++ Zaliczenie/switch.cpp
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include<stdio.h>
+
+int main()
+{
+	char opcja;
+	scanf_s("%c", &opcja);
+
+	switch (opcja) {
+	case 'a': {
+		printf("A");
+		break;
+	}
+	}
+
+
+	return 0;
+}
