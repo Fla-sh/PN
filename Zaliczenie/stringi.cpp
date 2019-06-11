e9de83da3eed7481e22fb71b6d4658b11555d450
Piotr <48599434+Fla-sh@users.noreply.github.com>
2019-06-11 18:10:35 +0200

    Update stringi.cpp

--- Zaliczenie/stringi.cpp
+++ Zaliczenie/stringi.cpp
@@ -6,20 +6,23 @@ int main()
 	
 	char a[20] = "SDfsdf";
 	a = "sdf"; // nie
-	strcpy(a, "dfsfd"); //tak
+	strcpy_s(a, "dfsfd"); //tak
+	// strcpy nie dziala bo Windows (y)
 	
 	char napis1[100] = "napis1";
 	char napis2[100] = "napis2";
 
 	printf("%s %s \n", napis1, napis2);
-
+	
 	char napis_z_konsoli[20];
 	scanf_s("%s", napis_z_konsoli, 20);
 	printf("%s\n", napis_z_konsoli);
-
+	
+	// dolacza napis2 do napis1
 	strcat_s(napis1, napis2);
 	printf("%s", napis1);
-
+	
+	// wskaznik na pierwsze wystapienie
 	char* miejsce = strchr(napis1, 'a');
 	printf("%d", miejsce);
 
